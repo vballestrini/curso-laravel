576dab6848a7ba1f854ac439101b6186451b1254
Víctor Ballestrini <vsballestrini@gmail.com>
2019-05-31 14:08:22 -0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/ola', function () {
+    $series = [
+        'Hannibal',
+        'Big Mouth',
+        'Breaking Bad'
+    ];
+    
+    $html = "<ul>";
+    foreach ($series as $serie){
+        $html .= "<li>$serie</li>";
+    }
+    $html .= "</ul>";
+
+    return $html;
+});
